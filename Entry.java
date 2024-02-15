f418be27620c16cfda4f889a8e3f61a4e36c5cdf
Mohamed O. Haroun Zarkik <mohamedhzfp@gmail.com>
2024-02-15 13:06:07 +0000

    First commit

---
+++ Entry.java
@@ -0,0 +1,17 @@
+class Entry {
+    private Object key;
+    private Object value;
+
+    public Entry(Object key, Object value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public Object getValue() {
+        return this.value;
+    }
+
+    public Object getKey() {
+        return this.value;
+    }
+}

5782192914b5d64beb8174faf1cf1744bb2b780b
Mohamed O. Haroun Zarkik <mohamedhzfp@gmail.com>
2024-02-21 17:37:03 +0000

    Modify addElement
    Added collision resolution through probing

--- Dictionary.java
+++ Dictionary.java
@@ -1,7 +1,8 @@
 import java.util.Arrays;
 
 public class Dictionary {
-    
+    private final int UNUSED = -1;
+    private final int DUMMY = -2;
     private int capacity = 8;
     private int index;
     private int[] indexes;
@@ -10,31 +11,39 @@ public class Dictionary {
 
     public Dictionary() {
         indexes = new int[capacity];
+        Arrays.fill(indexes, UNUSED);
         values = new Entry[(int) Math.round(capacity * (2.0/3))];
         index = 0;
         mask = capacity - 1;
     }
 
-    public void addElement(Entry value) {
-        int pseudoKey = value.getHash() & mask;
-        if(indexes[pseudoKey] == 0) {
-            indexes[pseudoKey] = ++index;
-            values[index - 1] = value;
-        } else {
-            int tempIndex = indexes[pseudoKey];
-            if(values[tempIndex - 1].getHash() == value.getHash()) {
-                values[tempIndex - 1] = value;
-            } 
+    public void addElement(Entry newEntry) {
+        int hash = newEntry.getHash();
+        int pseudoKey = hash & mask;
+        boolean stay = true;
+        while(stay) {
+            if(indexes[pseudoKey] == UNUSED) {
+                indexes[pseudoKey] = index;
+                values[index] = newEntry;
+                index++;
+                stay = false;
+            } else {
+                if(values[indexes[pseudoKey]].getHash() == newEntry.getHash()) {
+                    values[indexes[pseudoKey]] = newEntry;
+                }
+                hash >>= 5;
+                pseudoKey = (pseudoKey * 5 + hash + 1) & mask;
+            }
         }
     }
 
     public Object getElement(Object key) throws Exception {
         int pseudoKey = key.hashCode() & mask;
         int tempIndex = indexes[pseudoKey];
-        if(tempIndex == 0) {
+        if(tempIndex == UNUSED) {
             throw new Exception("Key not found");
         }
-        return values[tempIndex - 1];
+        return values[tempIndex];
     }
 }
 

1b67b2554bfbce16c78ceab77e3b9b21b1b3cbbc
Mohamed O. Haroun Zarkik <mohamedhzfp@gmail.com>
2024-03-02 13:12:03 +0000

    Add iterable interface to Keys

--- Keys.java
+++ Keys.java
@@ -1,6 +1,14 @@
-public class Keys {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Keys implements Iterable<Object>{
     private IndexesList indexes;
     private ValuesList values;
+
+    @Override
+    public Iterator<Object> iterator() {
+        return new KeysIterator(this);
+    }
 
     public Keys(IndexesList indexes, ValuesList values) {
         this.indexes = indexes;
@@ -9,5 +17,33 @@ public class Keys {
 
     public int length() {
         return values.size();
+    }
+
+    public class KeysIterator implements Iterator<Object> {
+        Keys keys;
+        int index;
+
+        public KeysIterator(Keys keys){
+            this.keys = keys;
+            this.index = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            if(index >= keys.values.length()) return false;
+            while(index <= keys.values.length()) {
+                if(keys.values.get(index) != null) return true;
+                index++;
+            }
+            return false;
+        }
+
+        @Override
+        public Object next() {
+            if (hasNext()) {
+                return keys.values.get(index++).getKey();
+            }
+            throw new NoSuchElementException();
+        }
     }
 }

0b1d13247b2ca139c0451aa7d228c1e44da2eb4a
Mohamed O. Haroun Zarkik <mohamedhzfp@gmail.com>
2024-03-15 17:04:33 +0000

    KeyError now adds simple brackets if it is a char or a String

--- KeyError.java
+++ KeyError.java
@@ -2,7 +2,7 @@ public class KeyError extends RuntimeException {
     private Object key;
 
     public KeyError(Object key) {
-        super((key instanceof String || key instanceof Character) ? "'" : "" + key + ((key instanceof String || key instanceof Character) ? "'" : ""));
+        super(((key instanceof String || key instanceof Character) ? "'" + key + "'" : String.valueOf(key)));
         this.key = key.toString();
     }
 

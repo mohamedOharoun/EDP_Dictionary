6a8fb538bc3217cc89f2074527e9fa9215002428
Mohamed O. Haroun Zarkik <mohamedhzfp@gmail.com>
2024-03-29 15:11:15 +0000

    Remove javadoc directory

--- src/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Dictionary"},{"p":"<Unnamed>","l":"Items"},{"p":"<Unnamed>","l":"KeyError"},{"p":"<Unnamed>","l":"Keys"},{"p":"<Unnamed>","l":"Missing"},{"p":"<Unnamed>","l":"ObjectView"},{"p":"<Unnamed>","l":"Pair"},{"p":"<Unnamed>","l":"TypeError"},{"p":"<Unnamed>","l":"Values"}]
